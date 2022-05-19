116b19a3ff5485f5d54b688a3400c5261bb5d32f
Raphael Roy <roy.raphael.33@gmail.com>
2022-05-19 09:32:01 +0200

    feat : add article class

--- frontend/js/article.js
+++ frontend/js/article.js
@@ -1,3 +1,23 @@
 /**
  * Représentation du format d'un article ainsi que le formatage des dates pour affichage
  */
+
+class Article {
+    title = "";
+    publicationDate = "";
+    image = "";
+    content = "";
+
+    constructor(article) {
+        this.title = article.title;
+        this.publicationDate = article.publicationDate;
+        this.image = article.image;
+        this.content = article.content;
+    }
+
+    formatDate() {
+        let timestamp = Date.parse(this.publicationDate);
+        let formattedDate = new Date(timestamp);
+        return formattedDate.toLocaleDateString("fr");
+    }
+}
