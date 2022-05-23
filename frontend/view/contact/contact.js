8321e16d96be089907265060a3e9f7d16f969abd
Raphael Roy <roy.raphael.33@gmail.com>
2022-05-23 10:06:39 +0200

    feat : validate form

--- frontend/view/contact/contact.js
+++ frontend/view/contact/contact.js
@@ -1,3 +1,17 @@
 /**
  * Gère l'affichage et les interactions de la page de contact
  */
+
+let sendButton = document.getElementsByClassName("btn")[0];
+sendButton.addEventListener('click', function(event) {
+    var valid = true;
+    for(let input of document.querySelectorAll(".form input,.form textarea")){
+        valid &= input.reportValidity();
+        if(! valid){
+            break;
+        }
+    }
+    if(valid){
+        alert("Votre message a bien été envoyé.");
+    }
+});
